2722209415e5e65bda0687a8e6ff022b39c29460
Hugo Bjork <hugobjork@me.com>
2022-08-18 11:49:32 +0200

    create user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/userModel')
 
 // @desc   Register a new user
 // @route  /api/users
@@ -11,8 +14,32 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Please include all fields')
   }
+  // Find if user already exist
+  const userExists = await User.findOne({ email })
 
-  res.send('Register Route')
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new error('Invalid user data')
+  }
 })
 
 // @desc   login a new user
