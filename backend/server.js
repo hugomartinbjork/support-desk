3a25d2a9bb2b711828d82c96408d592e25e93cb3
Hugo Bjork <hugobjork@me.com>
2022-08-28 17:32:10 +0200

    3.0

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,6 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  //   res.send('Hello')
-  res.status(200).json({ message: 'Welcome to the Support Desk API' })
-})
-
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
@@ -28,9 +23,13 @@ if (process.env.NODE_ENV === 'production') {
   //Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) =>
-    res.sendFile(__dirname, '../', 'fontend', 'build', 'index.html')
-  )
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
+} else {
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  })
 }
 
 app.use(errorHandler)
